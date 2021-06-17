4b9e4900537b751be2877599719ac6234c63e126
Gaurav Lalchandani <glalchandanig@gmail.com>
2021-06-18 00:13:22 +0530

    Implemented thread pool to handle concurrent connections
     - Performs poorly than no concurrent threads.

--- lilcache/cache.py
+++ lilcache/cache.py
@@ -3,6 +3,7 @@ Everything important goes here
 """
 
 import os
+import threading
 import socket
 import pickle
 
@@ -17,34 +18,11 @@ END_LIMIT = b"<<=>>"
 NONE_VALUE = pickle.dumps(None)
 RESPONSE_OK = b"OK"
 RESPONSE_ERROR = b"ERR"
+cache = dict()
 
 
-def _handler(conn, cache_path, snapshot, expires):
-    pass
-
-
-def is_last(packet):
-    return packet.endswith(END_LIMIT)
-
-
-def manager(sock_path, cache_path, snapshot, expires, poolsize):
-    """
-    The manager thread.
-
-    poolsize is used to manage the persistent client connections to the
-    manager.  If there are more than desired connections, then we kill
-    the most infrequently used connections.
-
-    TODO: Later think about snapshot and expires!
-    """
-    cache = dict()
-
-    mgr = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    mgr.bind(sock_path)
-    mgr.listen()
-
+def handle_connection(conn, cache_path, snapshot, expires):
     while True:
-        conn, addr = mgr.accept()
         command = conn.recv(BUFFER_SIZE)
         packet = command
         while not is_last(command):
@@ -62,7 +40,7 @@ def manager(sock_path, cache_path, snapshot, expires, poolsize):
                 cache[key] = value
             except Exception as e:
                 payload = create_payload(RESPONSE_ERROR, BUFFER_SIZE,
-                                         pickle.dumps(e))
+                                            pickle.dumps(e))
             else:
                 payload = create_payload(RESPONSE_OK, BUFFER_SIZE)
         elif args[0] == b'POP':
@@ -76,12 +54,47 @@ def manager(sock_path, cache_path, snapshot, expires, poolsize):
             conn.send(p)
 
 
+def is_last(packet):
+    return packet.endswith(END_LIMIT)
+
+
+
+def manager(sock_path, cache_path, snapshot, expires, poolsize):
+    """
+    The manager thread.
+
+    poolsize is used to manage the persistent client connections to the
+    manager.  If there are more than desired connections, then we kill
+    the most infrequently used connections.
+
+    TODO: Later think about snapshot and expires!
+    """
+    mgr = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    mgr.bind(sock_path)
+    mgr.listen()
+
+    connection_pool = []
+
+    while True:
+        conn, addr = mgr.accept()
+        if len(connection_pool) <= poolsize:
+            # Handle it in a thread
+            th = threading.Thread(target=handle_connection,
+                                  args=(conn, cache_path, snapshot, expires))
+            th.setDaemon(True)
+            th.start()
+            connection_pool.append(th)
+            continue
+        else:
+            raise NotImplemented("poolsize should be equal or more than 'expected' "
+                                 "concurrency level")
+
 def init(
     cache_name=None,
     root=None,
     snapshot=None,
     expires=None,
-    poolsize=10
+    poolsize=20
 ):
     if not cache_name:
         cache_name = generate_cache_name()
@@ -97,13 +110,10 @@ def init(
     if os.path.exists(sock_path):
         return
 
-    import threading
     mgr = threading.Thread(target=manager,
                            args=(sock_path, cache_path, snapshot, expires, poolsize))
     mgr.setDaemon(True)
     mgr.start()
-    # manager(sock_path, cache_path, snapshot, expires, poolsize)
-    return
 
 
 def establish_connection():
@@ -112,10 +122,13 @@ def establish_connection():
 
     We should try to reuse connections (from the connection pool)
     """
+    if "connection" in STATE:
+        return STATE["connection"]
     sock_path = STATE["sock_path"]
     if os.path.exists(sock_path):
         conn = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         conn.connect(sock_path)
+        STATE["connection"] = conn
         return conn
 
 
